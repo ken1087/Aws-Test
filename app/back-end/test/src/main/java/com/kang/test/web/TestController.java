ce4414eb01fa715e227885be32f297fcc26f6ca6
in kang <ken940816@gmail.com>
2021-10-12 20:54:46 +0900

    TestCommit

---
+++ app/back-end/test/src/main/java/com/kang/test/web/TestController.java
@@ -0,0 +1,16 @@
+package com.kang.test.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@GetMapping("/")
+	public ResponseEntity<?> findAll() {
+		String test = "Do you build a SnowMan?";
+		return new ResponseEntity<String>(test, HttpStatus.OK);
+	}
+}
